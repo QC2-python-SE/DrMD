0697bd7221457701436365e4fcdf81fdc7e7d881
DKLewis-UCL <dillon.lewis.24@ucl.ac.uk>
2024-10-26 23:05:05 +0100

    Added gate_list.py file with skeleton outline of common gate definitions

---
+++ gate_list.py
@@ -0,0 +1,17 @@
+"""
+A file consisting of commonly found qubit gates.
+
+The gates are initialised objects of the 'UnitaryGate' class from the
+'unitary_gate.py' file, which the user can easily call upon.
+"""
+
+# imports
+
+
+# common gate matrices
+
+# e.g.   X_matrix = np.array(blah)
+
+# define gates
+
+#e.g. X = UnitaryGate(X_matrix)
