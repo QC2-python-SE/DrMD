8bd966022ccea73f9d7252d0ba2c4d59c0cd6a47
DKLewis-UCL <dillon.lewis.24@ucl.ac.uk>
2024-11-24 16:01:42 +0000

    Added getter functions to state class to return qubit state information.

--- qubit_state.py
+++ qubit_state.py
@@ -9,90 +9,115 @@ class QubitState:
     unitary operations or measurements.
 
     Attributes:
-        qb_matrix (numpy.ndarray): the matrix representation of the
-        current two-qubit state
-        qb_init (numpy.ndarray): the matrix representation of the
-        original two-qubit state
+        __qb_matrix (numpy.ndarray): the matrix representation of the
+        current two-qubit state. Private variable to make it immutable
+        outside of the class.
+        __qb_init (numpy.ndarray): the matrix representation of the
+        original two-qubit state. Private variable to make it immutable
+        outside of the class.
     """
-    # Constructor for one two-qubit input matrix
-    def __init__(self, matrix):
+
+    # Constructor for qubit state input matrices
+    def __init__(self, matrix1, matrix2 = None):  
         """
         Initialises the QubitState object, ensuring all requirements
-        of a valid quantum state are met.
+        of a valid quantum state are met. Constructs the two-qubit 
+        state from two single-qubit states, if necessary.
 
         Args:
-            matrix (list): A 4x1 matrix representation of a 
-            two-qubit state.
+            matrix1 (list): Matrix representation for the qubit state, or
+            only first qubit state.
+            matrix2 (list): A 2x1 matrix representation for the
+            second qubit state, if necessary.
         
         Raises:
-            TypeError: Checks if 'matrix' parameter is a list, tuple or 
+            TypeError: Checks if 'matrix' parameters are a list, tuple or 
             numpy array.
-            ValueError: Checks if 'matrix' parameter is a 4x1 matrix.
+            ValueError: Checks if 'matrix1' parameter is correct size.
+            ValueError: Checks if 'matrix2' parameter is correct size.
         """
-        # Type check for matrix
-        if not isinstance(matrix, (tuple, list, np.ndarray)):  
-            raise TypeError("The qubit state matrix must be a tuple, list or NumPy array.")
+        # Checks if there was a single two-qubit state input or two
+        # single-qubit inputs
+        if matrix2 == None:
+            matrix = matrix1
+
+            # Type check for matrix
+            if not isinstance(matrix, (tuple, list, np.ndarray)):  
+                raise TypeError("The qubit state matrix must be a tuple,\
+                                 list or NumPy array.")
+            
+            # Convert list or tuple to np array
+            if isinstance(matrix, (list, tuple)):
+                matrix = np.array(matrix)
+            
+            # Check dimensions of matrix parameter
+            if matrix.ndim != 1 or matrix.shape != (4,):
+                raise ValueError("The qubit state matrix should be a 4x1 matrix.")
         
-        # Convert list or tuple to np array
-        if isinstance(matrix, (list, tuple)):
-            matrix = np.array(matrix)
+        else:
+            # Type check for matrices
+            if not isinstance((matrix1, matrix2), (tuple, list, np.ndarray)):  
+                raise TypeError("The qubit state matrices must be tuples, lists or NumPy arrays.")
+            
+            # Convert list or tuple to np array
+            if isinstance(matrix1, (list, tuple)):
+                matrix1 = np.array(matrix1)
+
+            if isinstance(matrix2, (list, tuple)):
+                matrix2 = np.array(matrix2)
+            
+            # Check dimensions of matrix parameters
+            if matrix1.ndim != 1 or matrix1.shape != (2,):
+                raise ValueError("The first qubit state matrix should be a 2x1 matrix.")
+            
+            if matrix2.ndim != 1 or matrix2.shape != (2,):
+                raise ValueError("The second qubit state matrix should be a 2x1 matrix.")
         
-        # Check dimensions of matrix parameter
-        if matrix.ndim != 1 or matrix.shape != (4,):
-            raise ValueError("The qubit state matrix should be a 4x1 matrix.")
-        
+            # Combine the two single-qubit states
+            matrix = np.kron(matrix1, matrix2)
+
         # Check normalisation
         total_sum = np.sum(matrix)
         if not np.isclose(total_sum, 1.0, atol=1e-7):  # tolerance for floating-point errors
             matrix = matrix/total_sum  # renormalise
         
-        self.qb_matrix = matrix
+        self.__qb_matrix = matrix
+        self.__qb_init = matrix  # storing initial state in private variable
 
-    # Constructor for two one-qubit input matrices
-    def __init__(self, matrix1, matrix2):  
+    def get_initial(self):
         """
-        Initialises the QubitState object, ensuring all requirements
-        of a valid quantum state are met. Constructs the two-qubit 
-        state from two single-qubit states.
-
-        Args:
-            matrix1 (list): A 2x1 matrix representation for the
-            first qubit state.
-            matrix2 (list): A 2x1 matrix representation for the
-            second qubit state.
+        Function to give the qubit state used to initialise this
+        QubitState object.
         
-        Raises:
-            TypeError: Checks if 'matrix' parameters are a list, tuple or 
-            numpy array.
-            ValueError: Checks if 'matrix1' parameter is a  2x1 matrix.
-            ValueError: Checks if 'matrix2' parameter is a  2x1 matrix.
+        Returns:
+            numpy.ndarray: The initial qubit state.
         """
-        
-        # Type check for matrices
-        if not isinstance((matrix1, matrix2), (tuple, list, np.ndarray)):  
-            raise TypeError("The qubit state matrices must be tuples, lists or NumPy arrays.")
-        
-        # Convert list or tuple to np array
-        if isinstance(matrix1, (list, tuple)):
-            matrix1 = np.array(matrix1)
+        return self.__qb_init
+    
+    def peek(self):
+        """
+        Function to show the current qubit state probabilities in the
+        computational basis.
 
-        if isinstance(matrix2, (list, tuple)):
-            matrix2 = np.array(matrix2)
-        
-        # Check dimensions of matrix parameters
-        if matrix1.ndim != 1 or matrix1.shape != (2,):
-            raise ValueError("The first qubit state matrix should be a 2x1 matrix.")
-        
-        if matrix2.ndim != 1 or matrix2.shape != (2,):
-            raise ValueError("The second qubit state matrix should be a 2x1 matrix.")
-        
-        # Combine the two single-qubit states
-        complete_state = np.kron(matrix1, matrix2)
+        Returns:
+            numpy.ndarray: 4x1 matrix of current qubit state.
+        """
+        return self.__qb_matrix
 
-        # Check normalisation
-        total_sum = np.sum(complete_state)
-        if not np.isclose(total_sum, 1.0, atol=1e-7):  # tolerance for floating-point errors
-            complete_state= complete_state/total_sum  # renormalise
-        
-        self.qb_matrix = complete_state
+    def __repr__(self):
+        """
+        Function to override the default 'print()' behaviour in python.
+        Calling 'print()' on a QubitState object, returns the current
+        qubit state matrix.
+
+        Returns:
+            str: 4x1 matrix of current qubit state.
+        """
+        return str(self.peek())
+    
+
+    
+
+
+    
 
