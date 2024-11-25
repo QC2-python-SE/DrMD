372c8052f9a0d1d4a83c65d53628c6ac735babbf
phamncmai <ucaqnph@ucl.ac.uk>
2024-11-25 18:18:41 +0000

    Added 1 unitary_gate test

--- test_unitary.py
+++ test_unitary.py
@@ -5,7 +5,13 @@ import unitary_gate as ug
 x = np.array([0, 1])
 y = np.array([1, 0])
 
-print(gl.H_mat)
-
 xmat = ug.UnitaryGate(gl.X_mat)
-print(xmat.apply(y))
+
+def test_unitary_apply():
+    # Test unitary_gate.apply function
+    l = x == xmat.apply(y)
+    assert l.all()
+
+
+
+test_unitary_apply()
