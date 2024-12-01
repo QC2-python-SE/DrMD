5e03173aacb7012a2cd2859cf725677915060ffa
DKLewis-UCL <dillon.lewis.24@ucl.ac.uk>
2024-12-01 18:07:19 +0000

    Corrected email addresses in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
    name='drmd',
    version='1.0',
-   description='A python package for performing gate operations on two qubits.',
+   description='A python package for performing quantum gate operations on two qubits.',
    long_description=open("README.md").read(),          
    long_description_content_type="text/markdown",   
    author='DrMD',
-   author_email='dillon.lewis.24@ucl.ac.uk',
+   author_email=('dillon.lewis.24@ucl.ac.uk', 'nguyet.pham.24@ucl.ac.uk', 'ralph.costales.24@ucl.ac.uk', 'delia.citea.20@ucl.ac.uk'),
    url='https://github.com/QC2-python-SE/DrMD',
    license='MIT',
    packages=find_packages(),
